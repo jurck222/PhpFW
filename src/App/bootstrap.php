d31fb42e96f71e84da8e80355d3a2c0f2ed127ec
jurck222 <56190152+jurck222@users.noreply.github.com>
2023-11-14 16:46:19 +0100

    router update

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,10 +2,16 @@
 
 declare(strict_types=1);
 
-require __DIR__."/../../vendor/autoload.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Config\Paths;
 
-$app = new App();
+use function App\Config\registerRoutes;
+
+
+$app = new App(Paths::SOURCE . "app/container-definitions.php");
+
+registerRoutes($app);
 
 return $app;
---
+++ src/App/views/partials/_footer.php
@@ -0,0 +1,7 @@
+<footer class="container mx-auto my-12 text-center">
+    &copy; PhpFW
+</footer>
+<!-- End Footer -->
+</body>
+
+</html>
